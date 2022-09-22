118835b45e4eaeb15d0daa30190b6725155c6c98
hkd92 <hassanekadridodo@gmail.com>
2022-09-22 06:44:24 +0200

    [ADD] commentaire

--- front/js/script.js
+++ front/js/script.js
@@ -1,10 +1,16 @@
+//Recuperer la réponse de l'api
 fetch("http://localhost:3000/api/products").then(res => res.json())
 .then(data =>{
+    // Création des items produits
     for (let i = 0; i < data.length; i++) {
+        // Création d'un élément de type "a"
         let anchor = document.createElement("a");
         anchor.href = "html/product.html?id="+data[i]._id;
+        // Création d'un élément de type "article"
         let articleElement = document.createElement("article");
+        // Création d'un élément de type "image"
         const imageElement = document.createElement("img");
+        // On accède à l’indice i de la liste items pour configurer la source de l’image.
         imageElement.src = data[i].imageUrl;
         imageElement.alt = data[i].altTxt;
         const productName = document.createElement("h3");
@@ -12,13 +18,15 @@ fetch("http://localhost:3000/api/products").then(res => res.json())
         const productDescription = document.createElement("p");
         productDescription.innerHTML = data[i].description;
         
+        // On rattache l’image à articleElement (la balise article)
         articleElement.appendChild(imageElement);
         articleElement.appendChild(productName);
         articleElement.appendChild(productDescription);
-   
         anchor.appendChild(articleElement);
         
+        // Récupération de l'élément du DOM qui accueillera les items
         const pieceElement = document.querySelector(".items");
+        // On rattache le lien à pieceElement (la balise "a")
         pieceElement.appendChild(anchor);
         
     }
