a4f5bf80c9b11ccb2f783ccee49178233828720b
hkd92 <hassanekadridodo@gmail.com>
2022-09-22 06:37:26 +0200

    [ADD] products_js

--- front/js/script.js
+++ front/js/script.js
@@ -2,7 +2,7 @@ fetch("http://localhost:3000/api/products").then(res => res.json())
 .then(data =>{
     for (let i = 0; i < data.length; i++) {
         let anchor = document.createElement("a");
-        //anchor.href = "html/product.html";
+        anchor.href = "html/product.html?id="+data[i]._id;
         let articleElement = document.createElement("article");
         const imageElement = document.createElement("img");
         imageElement.src = data[i].imageUrl;
@@ -12,7 +12,6 @@ fetch("http://localhost:3000/api/products").then(res => res.json())
         const productDescription = document.createElement("p");
         productDescription.innerHTML = data[i].description;
         
-        const listeElement = [imageElement, productName, productDescription];
         articleElement.appendChild(imageElement);
         articleElement.appendChild(productName);
         articleElement.appendChild(productDescription);
