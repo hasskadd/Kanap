e46a473fa0316de72aca29350a69fd5ccfd7ae97
hkd92 <hassanekadridodo@gmail.com>
2022-09-25 20:00:44 +0200

    gestion du panier

--- front/js/cart.js
+++ front/js/cart.js
@@ -11,3 +11,30 @@ for (let i = 0; i < localStorage.length; i++) {
  
 }
 console.log(jsonArray);
+
+for(let key in jsonArray){
+    console.log(jsonArray[key].id);
+    cartItems.innerHTML += 
+        `<article class="cart__item" data-id=${jsonArray[key].id} data-color=${jsonArray[key].color}>
+            <div class="cart__item__img">
+                <img src=${jsonArray[key].src} alt=${jsonArray[key].altTxt}>
+            </div>
+            <div class="cart__item__content">
+                <div class="cart__item__content__description">
+                    <h2>${jsonArray[key].name}</h2>
+                    <p>${jsonArray[key].color}</p>
+                    <p>${jsonArray[key].price} €</p>
+                </div>
+                <div class="cart__item__content__settings">
+                    <div class="cart__item__content__settings__quantity">
+                        <p>Qté : ${jsonArray[key].quantity}</p>
+                        <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
+                    </div>
+                    <div class="cart__item__content__settings__delete">
+                        <p class="deleteItem">Supprimer</p>
+                    </div>
+                </div>
+            </div>
+        </article>`
+}
+
